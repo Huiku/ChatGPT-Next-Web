1a39748f51fff92db0cd1be09e6289ee85f8afd2
haohui <hhh915@126.com>
2024-10-30 12:46:24 +0800

    fix:调整

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -230,8 +230,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="ChatGPT"
+        subTitle="发财就手"
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
